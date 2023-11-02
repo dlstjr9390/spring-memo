66c92a7d14238103301f23bbcc80241e1e1b8a08
Yoon <dlstjr9390@gmail.com>
2023-11-02 15:10:07 +0900

    Update, Delete 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -3,6 +3,7 @@ package com.sparta.memo.controller;
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -40,5 +41,34 @@ public class MemoController {
                 .map(MemoResponseDto::new).toList();
 
         return responseList;
+    }
+
+    @SneakyThrows
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){
+        // 해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)) {
+            //해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else{
+            throw new IllegalAccessException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @SneakyThrows
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id){
+        //해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            //해당 메모를 삭제하기
+            memoList.remove(id);
+            return id;
+        } else{
+            throw new IllegalAccessException("선택한 메모는 존재하지 않습니다.");
+        }
     }
 }
